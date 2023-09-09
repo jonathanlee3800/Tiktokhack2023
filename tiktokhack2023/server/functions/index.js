128e1201b9476842a74bcbd0035223f7e04a94ea
jonathanlee3800 <jonathannnleee@gmail.com>
2023-09-09 21:55:22 +0800

    firebase deploy

--- tiktokhack2023/server/functions/index.js
+++ tiktokhack2023/server/functions/index.js
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
@@ -20,11 +20,9 @@ const logger = require("firebase-functions/logger");
 
 const express = require("express");
 const functions = require("firebase-functions");
-require("dotenv").config();
+// require("dotenv").config();
 
-// const { getAllCampaignsByUser } = require("./controllers/shelfController.js");
-
-
+const { getAllCampaignsByUser } = require("./controllers/shelfController.js");
 
 const app = express();
 const port = 3000;
@@ -33,12 +31,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// app.get("/user/shelf/:userId", getAllCampaignsByUser);
+app.get("/user/shelf/:userid", getAllCampaignsByUser);
 
 // app.listen(port, async () => {
 //   console.log(`Example app listening on port ${port}`);
 // });
 
 exports.app = functions.https.onRequest(app);
-
-
